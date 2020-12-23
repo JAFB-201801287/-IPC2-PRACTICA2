ef5358d1b3e1613d188b7544926072b32c3da712
JAFB-201801287 <75402151+JAFB-201801287@users.noreply.github.com>
2020-12-23 17:33:37 -0600

    TABLA PAIS

--- DB.sql
+++ DB.sql
@@ -1,2 +1,40 @@
+/*
+Los hoteles cuentan con una diversa cantidad de habitaciones divididas por precio y comodidades
+según el cliente desee.
+La cantidad y calidad de habitaciones dependerá la calidad del hotel, la cadena de hotelería maneja
+hoteles de categorías a partir de las tres estrellas hasta las cinco estrellas.
+Para un hotel de tres estrellas se cuenta con cuarenta habitaciones divididas en dos tipos de
+habitaciones:
+- Habitación simple: Cuenta con dos camas imperiales, equipada con baño, TV digital,
+teléfono y maquina cafetera.
+- Habitación individual: Cuenta con una cama king, un sofá cama y se encuentra equipada
+con baño, TV digital, aire acondicionado, teléfono, maquina cafetera y cajilla de seguridad.
+Los hoteles de cuatro y cinco estrellas cuentan con cincuenta y sesenta habitaciones
+respectivamente, para ambos se divide en dos tipos de habitaciones.
+- Habitación simple: Cuenta con dos camas dobles, equipada con baño, TV digital, teléfono
+y maquina cafetera.
+- Habitación individual: Cuenta con una cama king, puede o no tener un balcón, un sofá cama
+y se encuentra equipada con baño, TV digital, aire acondicionado, teléfono, maquina
+cafetera y cajilla de seguridad.
+Para el ingreso a las habitaciones se deben registrar tanto las entradas como las salidas de los
+huéspedes tomando en cuenta que, el ckeck-in se realiza a partir de las tres de la tarde mientras
+el check-out se realiza a medio día.
+Si el cliente realiza el check-out pasado medio día, se le cobrará una mora por hora de atraso,
+mientras que la hora del check-in es inamovible, sin embargo, se 
+*/
+
+/*
+PAIS
+# Codigo de pais
+* Nombre
+*/
+
+CREATE DATABASE practica2;
+USE practica2;
+
+CREATE TABLE Pais (
+	id_pais INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nombre INT  NOT NULL
+);
 
 
